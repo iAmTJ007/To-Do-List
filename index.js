6fef351c59474fce7f3fb55e27833447fccd2d81
Tathagya <eveninghappy11@gmail.com>
2023-12-15 23:41:03 +0530

    completed,delete remaining

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const area=document.getElementById("tasks");
 const addtask=()=>{
     const textval=document.getElementById("textbox").value;
     const newdiv=document.createElement('div');
-    newdiv.textContent=textval;
+    newdiv.className="newdiv"
+    newdiv.innerHTML=`
+                <img src="hook-1727484_1920.png" onclick="completetask()" alt="">
+                <p>${textval}</p>
+                <img src="remove.png" onclick="deletetask()" alt="">       
+    `;
     area.appendChild(newdiv);
 };
+const completetask=()=>{
+    
+}
